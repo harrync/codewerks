c0d8af2bcfa82437158b0a23996353a0275c5c11
Lucas Yeats <58042433+annandaledigital@users.noreply.github.com>
2022-05-11 15:01:20 +0100

    testing

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Codewerks</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Something new and fancy is coming your way soon!</code>
         </p>
       </main>
 
